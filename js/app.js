1d10feb163947b984e54866687b4daff913870b3
ageronimo <angelageroniimo@gmail.com>
2017-04-10 09:20:40 -1000

    added ids to buttons| got points adding to work (on second answer only though)

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
 var num = 0;
 
-function plusOnePoint(){
-	var pointReader = document.getElementById("harrisLikes1");
-	console.log(pointReader);
-	var num = 0;
-	num + 1;
+
+//make dynamic id by adding 'like' or 'dislike' and the last name 
+//if the id already exists, add a number (would be used for multiple comments by user)
+function plusOnePoint(id){
+  console.log("hello. i work :)");
+  var pointReader = document.getElementById(id);
+  console.log(id);
+  console.log(num);
+  num = num + 1;
 	pointReader.innerHTML = "+" + num;
-	console.log(num);
 }
