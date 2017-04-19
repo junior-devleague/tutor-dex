120a8bc2d4ec0a96857f9b8ed8dd7e915a555466
ageronimo <angelageroniimo@gmail.com>
2017-04-19 09:18:06 -1000

    attempted to pseudo code point system

--- js/app.js
+++ js/app.js
@@ -2,8 +2,22 @@ var num = 0;
 var numTwo = 0;
 var disliked = [];
 
-//make dynamic id by adding 'like' or 'dislike' and the last name 
-//if the id already exists, add a number (would be used for multiple comments by user)
+/*
+-make dynamic id by adding 'like' or 'dislike' and the last name 
+-if the id already exists, add a number (would be used for multiple comments by user)
+-if points are negative, turn button red
+*/
+
+/* POINT CHANGING
+idea 1:
+  id the buttons with numbers (i.e. l1 dl1 l2 dl2)
+  use to target class of button with points 
+    ex. document.getElementsByClassName("buttonClass[id]").innerHTML
+  change inner html to add or subtract points
+  check for positive and negative points
+  if total is negative, make button style red
+*/
+
 function plusOnePoint(id){
   console.log("hello. i work :)");
   var pointReader = document.getElementById(id);
