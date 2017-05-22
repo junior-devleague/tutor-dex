eb296b664125e09fb20180c3c47aebcc48f096a5
ageronimo <angelageroniimo@gmail.com>
2017-05-22 08:37:31 -1000

    fixed list sorting

--- js/app.js
+++ js/app.js
@@ -207,13 +207,13 @@ function plusSecondPoint(id){
 
 /****************************SORTING**********************************/
 /* taken from https://www.w3schools.com/howto/howto_js_sort_list.asp */
-
+/*
 function sortList() {
   var list, i, switching, b, shouldSwitch;
   list = document.getElementById("list");
   switching = true;
   /*Make a loop that will continue until
-  no switching has been done:*/
+  no switching has been done:
   while (switching) {
     //start by saying: no switching is done:
     switching = false;
@@ -223,22 +223,22 @@ function sortList() {
       //start by saying there should be no switching:
       shouldSwitch = false;
       /*check if the next item should
-      switch place with the current item:*/
+      switch place with the current item:
       if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
         /*if next item is alphabetically
         lower than current item, mark as a switch
-        and break the loop:*/
+        and break the loop:
         shouldSwitch= true;
         break;
       }
     }
     if (shouldSwitch) {
       /*If a switch has been marked, make the switch
-      and mark the switch as done:*/
+      and mark the switch as done:
       b[i].parentNode.insertBefore(b[i + 1], b[i]);
       switching = true;
     }
   }
 }
 
-window.onload = sortList;
+window.onload = sortList;*/
